b8437a99c52297a934dcee26a1ba890fefa38ee6
munyao <victor.mulinge@tangazoletu.com>
2025-09-07 14:40:37 +0300

    removed hard coded routes, added a new route class

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -3,6 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
 
+import 'package:mynotes/constants/routes.dart';
+
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
 
@@ -63,7 +65,7 @@ class _LoginViewState extends State<LoginView> {
                   );
                   Navigator.of(
                     context,
-                  ).pushNamedAndRemoveUntil('/notes/', (route) => false);
+                  ).pushNamedAndRemoveUntil(notesRoute, (route) => false);
                 } on FirebaseAuthException catch (e) {
                   if (e.code == 'user-not-found') {
                     devtools.log('user not found');
@@ -78,7 +80,7 @@ class _LoginViewState extends State<LoginView> {
               onPressed: () {
                 Navigator.of(
                   context,
-                ).pushNamedAndRemoveUntil('/register/', (route) => false);
+                ).pushNamedAndRemoveUntil(registerRoute, (route) => false);
               },
               child: const Text('Not registered yet? Register here'),
             ),
