36fda96b5f2de0e968ce5b5c57f5c6cd00fa2f38
munyao <victor.mulinge@tangazoletu.com>
2025-09-08 05:10:38 +0300

    added a show error dialog class and implemented its functionality

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,9 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
 
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -68,10 +68,14 @@ class _LoginViewState extends State<LoginView> {
                   ).pushNamedAndRemoveUntil(notesRoute, (route) => false);
                 } on FirebaseAuthException catch (e) {
                   if (e.code == 'user-not-found') {
-                    devtools.log('user not found');
+                    await showErrorDialog(context, 'User not found');
                   } else if (e.code == 'wrong-password') {
-                    devtools.log('wrong password');
+                    await showErrorDialog(context, 'Wrong credentials');
+                  } else {
+                    await showErrorDialog(context, 'Error: ${e.code}');
                   }
+                } catch (e) {
+                  await showErrorDialog(context, e.toString());
                 }
               },
               child: const Text('Login'),
